b0fd18e4ac0a20721f22eb3ba5c6dc5bee3db3d2
mmarquezs <mmsa1994@gmail.com>
2014-10-25 18:11:17 +0200

    Test file added by mistake deleted.

--- test.py
+++
@@ -1,6 +0,0 @@
-import urllib2,urllib
-import json
-imdb_id='tt0111161'
-response = urllib2.urlopen("http://www.myapifilms.com/imdb?idIMDB=%s&lang=es-es" % urllib.quote_plus(imdb_id))
-data = json.load(response)
-print(data['title'])
